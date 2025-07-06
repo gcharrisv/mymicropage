29263e82612e652ed592fb7cc9f228a5937622b7
George <gcharris@calpoly.edu>
2025-07-06 02:43:21 -0700

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,6 @@ const App = () => (
       <BrowserRouter basename="/mymicropage">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
